5225544cd642278e188f6dfd82ada6d0df64a714
kipp7 <13377035712@163.com>
2025-03-13 13:41:27 +0800

    feat: 初始化 Ant Design Pro 项目

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # landslide-monitor
+=======
+# README
+
+`@umijs/max` 模板项目，更多功能参考 [Umi Max 简介](https://umijs.org/docs/max/introduce)
+>>>>>>> b1ed5cc (feat: 初始化 Ant Design Pro 项目)
---
+++ src/services/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://sdssoyyjhunltmcjoxtg.supabase.co';
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkc3NveXlqaHVubHRtY2pveHRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MzY3NTIsImV4cCI6MjA1NzAxMjc1Mn0.FisL8HivC_g-cnq4o7BNqHQ8vKDUpgfW3lUINfDXMSA';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
